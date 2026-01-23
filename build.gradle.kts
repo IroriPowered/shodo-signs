773539c59c8b6139f2f6ff0194ef64f67ccf8ea0
KabanFriends <yuukun2004@protonmail.ch>
2026-01-23 16:01:49 +0900

    build: 📦 use official HytaleServer maven artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,14 @@ version = "1.0.5-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.hytale.com/release")
+    maven("https://maven.hytale.com/pre-release")
     maven("https://maven.irori.cc/repository/public/")
     maven("https://maven.hytale-modding.info/releases/")
 }
 
 dependencies {
-    compileOnly(files("libs/HytaleServer.jar"))
+    compileOnly(libs.hytale)
     compileOnly(libs.shodo)
     compileOnly(libs.multiplehud)
 }
